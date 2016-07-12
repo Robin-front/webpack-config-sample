86c364ddf7d6f2e2cac217fa6817169942812fca
robin <lb.robin1991@gmail.com>
2016-07-12 21:02:00 +0800

    forgetPwd

---
+++ src/page/forgetPwd/index.js
@@ -0,0 +1,23 @@
+import './forgetPwd.html'
+
+import { countdown } from 'components/utils'
+
+$(function(){
+
+  // 获取验证码倒计时
+  $(document).on('click', '.J_get_code', function(){
+    var _maxNum = 60; //倒计时数字
+    var _this = $(this);
+    if(!_this.hasClass("disable")){
+        _this.addClass("disable");
+        _this.html("剩余 "+_maxNum+" s");
+        countdown(_maxNum, function(_num){
+            _this.html("剩余 "+_num+" s");
+        },function(){
+            _this.html("重新获取");
+            _this.removeClass("disable");
+        });
+    }
+  });
+
+});
