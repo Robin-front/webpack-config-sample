fae8397a848c839f3356228e6b0e2d09855c017e
robin <lb.robin1991@gmail.com>
2016-11-30 11:36:44 +0800

    my

--- src/page/my/index.js
+++ src/page/my/index.js
@@ -1,2 +1,25 @@
 import './my.html'
 import './my.less'
+
+$(function(){
+  $(".picker-age").calendar({});
+
+  $('.save-age').on('click', function(){
+    var value = $('.picker-age').val();
+
+    $.ajax({
+      url: '/',
+      type: 'post',
+      data: {
+        date: value
+      },
+      success: function(data){
+        if (data.success == 1) {
+          var age = new Date().getFullYear() - value.split('-')[0];
+          $('.picker-age-val').text(age);
+          $.closeModal('.popup-age');
+        }
+      }
+    })
+  });
+})
